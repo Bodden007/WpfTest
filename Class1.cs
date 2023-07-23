e3597bd41df6d9586455c7dcc55e73350c3ad174
Bodden007 <denis-bodrov@mail.ru>
2023-07-23 22:37:26 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Threading;
+
+namespace WpfTest
+{
+    public class Class1 : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int data;
+        private bool one = true;
+        private bool two = false;
+        private bool three = false;
+        
+        public int Data1
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public int Data2
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public int Data3
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public void Foo()
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                if  (one)
+                {
+                    Data1 = i;
+                    one = false;
+                    two = true;
+                    OnPropertyChanged(nameof(Data1));
+                }else if (two)
+                {
+                    Data2 = i;
+                    two = false;
+                    three = true;
+                    OnPropertyChanged(nameof(Data2));
+                }else if (three)
+                {
+                    Data3 = i;
+                    three = false;
+                    one = true;
+                    OnPropertyChanged(nameof(Data3));
+                }
+                
+                
+                Thread.Sleep(500);
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = " ")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public Class1 MyClass1 { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Class1 class1 = new Class1();
+
+            Task.Run(() =>
+            {
+                class1.Foo();
+            });
+
+            DataContext = class1;
+        }
+
+        
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Привет", "Привет");
+        }
+
+
+        public void FirstMethod(Object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Z", "Привет");
+        }
+
+        public void SecondMethod(Object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("H", "Привет");
+        }
+    }
+}
